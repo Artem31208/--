c7993fef2b44ec8015764ffb3920ece705e06fd1
Artem <branchenko01@ukr.net>
2022-12-05 14:48:16 +0200

    sdfgh

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿string str;
+Console.WriteLine("Скільки тобі років?");
+str = Console.ReadLine();
+int a = Convert.ToInt32(str);
+if (a == 18)
+{
+    Console.WriteLine(a == 18);
+}
+else if (a >= 18)
+{
+    Console.WriteLine(a>=18);
+}
+if (a<18)
+{
+    Console.WriteLine("Малолетка");
+}
