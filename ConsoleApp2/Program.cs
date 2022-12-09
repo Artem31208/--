b3e9c118d47d38f8c55224b048b0df9193f10a70
Dmytro Lytvynenko <dmytro.lytvynenko@fortius-network.com>
2022-12-09 12:48:51 +0200

    Code cleaning

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,12 +1,9 @@
-﻿string str;
+﻿string? str;
 Console.WriteLine("Скільки тобі років?");
 str = Console.ReadLine();
 int a = Convert.ToInt32(str);
-if (a == 18)
-{
-    Console.WriteLine(a == 18);
-}
-else if (a >= 18)
+
+if (a >= 18)
 {
     Console.WriteLine(a>=18);
 }
