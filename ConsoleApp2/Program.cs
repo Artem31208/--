e735cefd4b51d695b7a998b415af30142cba981b
Artem <branchenko01@ukr.net>
2022-12-16 17:47:33 +0200

    Дороботка

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -4,19 +4,22 @@ string mane = Console.ReadLine();
 Console.WriteLine("Скажи своє Прізвище");
 string lastname = Console.ReadLine();
 Console.WriteLine("Скільки тобі років?");
-str = Console.ReadLine();
+str  = Console.ReadLine();
 int a = Convert.ToInt32(str);
 if (a == 18)
 {
     Console.WriteLine("Повнолітній");
-}
+
 else if (a >= 18)
 {
     Console.WriteLine("Повнолітній");
+   
 }
 if (a < 18)
 {
-    Console.WriteLine("Неповнолітній");
+    
+   Console.WriteLine ("Неповнолітній");
+
 }
 Console.WriteLine("Скільки ти витратив вчора?");
 double e, d;
@@ -27,7 +30,6 @@ double rezult = e + d;
 Console.WriteLine("Разом за два дні витратив = " + rezult);
 Console.ReadLine();
 Console.Clear();
-Console.WriteLine (mane); 
-Console.WriteLine(lastname);
+Console.WriteLine (mane  +  lastname);
 Console.WriteLine(a);
-Console.WriteLine(rezult);
+Console.WriteLine("Разом витратив " + rezult);
